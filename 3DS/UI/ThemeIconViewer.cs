cc706770f0b5537df4ff74500389e437f32b40e5
Clienthax <clienthax@gmail.com>
2014-11-14 19:31:46 +0000

    Add support for 3ds theme .icn files.
    Signed-off-by: Clienthax <clienthax@gmail.com>

---
+++ 3DS/UI/ThemeIconViewer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _3DS.UI
+{
+    public partial class ThemeIconViewer : Form
+    {
+        ThemeIconFile themeIcon;
+        public ThemeIconViewer(ThemeIconFile themeIcon)
+        {
+            this.themeIcon = themeIcon;
+            InitializeComponent();
+        }
+
+        private void ThemeIconViewer_Load_1(object sender, EventArgs e)
+        {
+            pictureBox1.Image = themeIcon.getIconTexture();
+        }
+    }
+}
