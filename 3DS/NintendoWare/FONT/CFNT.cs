a0a4ba3ce8b1b7b6a98be3314fff79486d78356f
Gericom <f.nouwt@dshack.org>
2014-11-17 19:10:27 +0100

    More CFNT stuff

--- 3DS/NintendoWare/FONT/CFNT.cs
+++ 3DS/NintendoWare/FONT/CFNT.cs
@@ -6,6 +6,7 @@ using LibEveryFileExplorer.Files;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using _3DS.GPU;
 
 namespace _3DS.NintendoWare.FONT
 {
@@ -18,7 +19,32 @@ namespace _3DS.NintendoWare.FONT
 			{
 				Header = new CFNTHeader(er);
 				FontInfo = new FINF(er);
-				//TextureGlyph = new TGLP(er);
+				er.BaseStream.Position = FontInfo.TGLPOffset - 8;
+				TextureGlyph = new TGLP(er);
+
+				List<CWDH> tmp = new List<CWDH>();
+				er.BaseStream.Position = FontInfo.CWDHOffset - 8;
+				CWDH Last;
+				do
+				{
+					Last = new CWDH(er);
+					tmp.Add(Last);
+					if (Last.NextCWDHOffset != 0) er.BaseStream.Position = Last.NextCWDHOffset - 8;
+				}
+				while (Last.NextCWDHOffset != 0);
+				CharWidths = tmp.ToArray();
+
+				List<CMAP> tmp2 = new List<CMAP>();
+				er.BaseStream.Position = FontInfo.CMAPOffset - 8;
+				CMAP Last2;
+				do
+				{
+					Last2 = new CMAP(er);
+					tmp2.Add(Last2);
+					if (Last2.NextCMAPOffset != 0) er.BaseStream.Position = Last2.NextCMAPOffset - 8;
+				}
+				while (Last2.NextCMAPOffset != 0);
+				CharMaps = tmp2.ToArray();
 			}
 			finally
 			{
@@ -55,7 +81,7 @@ namespace _3DS.NintendoWare.FONT
 		public FINF FontInfo;
 		public class FINF
 		{
-			public FINF(EndianBinaryReader er)//same as wii font's
+			public FINF(EndianBinaryReader er)
 			{
 				Signature = er.ReadString(System.Text.Encoding.ASCII, 4);
 				if (Signature != "FINF") throw new SignatureNotCorrectException(Signature, "FINF", er.BaseStream.Position - 4);
@@ -63,7 +89,7 @@ namespace _3DS.NintendoWare.FONT
 				FontType = er.ReadByte();
 				LineFeed = er.ReadByte();
 				AlterCharIndex = er.ReadUInt16();
-				DefaultWidth = new CharWidths(er);
+				DefaultWidth = new CharWidthInfo(er);
 				Encoding = er.ReadByte();
 				TGLPOffset = er.ReadUInt32();
 				CWDHOffset = er.ReadUInt32();
@@ -78,7 +104,7 @@ namespace _3DS.NintendoWare.FONT
 			public Byte FontType;
 			public Byte LineFeed;
 			public UInt16 AlterCharIndex;
-			public CharWidths DefaultWidth;
+			public CharWidthInfo DefaultWidth;
 			public Byte Encoding;
 			public UInt32 TGLPOffset;
 			public UInt32 CWDHOffset;
@@ -89,9 +115,105 @@ namespace _3DS.NintendoWare.FONT
 			public Byte Padding;//?
 		}
 
-		public class CharWidths
+		public TGLP TextureGlyph;
+		public class TGLP
 		{
-			public CharWidths(EndianBinaryReader er)
+			public TGLP(EndianBinaryReader er)
+			{
+				Signature = er.ReadString(Encoding.ASCII, 4);
+				if (Signature != "TGLP") throw new SignatureNotCorrectException(Signature, "TGLP", er.BaseStream.Position - 4);
+				SectionSize = er.ReadUInt32();
+				CellWidth = er.ReadByte();
+				CellHeight = er.ReadByte();
+				BaselinePos = er.ReadByte();
+				MaxCharWidth = er.ReadByte();
+				SheetSize = er.ReadUInt32();
+				NrSheets = er.ReadUInt16();
+				SheetFormat = (Textures.ImageFormat)er.ReadUInt16();
+				SheetRow = er.ReadUInt16();
+				SheetLine = er.ReadUInt16();
+				SheetWidth = er.ReadUInt16();
+				SheetHeight = er.ReadUInt16();
+				SheetDataOffset = er.ReadUInt32();
+
+				er.BaseStream.Position = SheetDataOffset;
+				Sheets = new byte[NrSheets][];
+				for (int i = 0; i < NrSheets; i++) Sheets[i] = er.ReadBytes((int)SheetSize);
+			}
+			public String Signature;
+			public UInt32 SectionSize;
+			public Byte CellWidth;
+			public Byte CellHeight;
+			public Byte BaselinePos;
+			public Byte MaxCharWidth;
+			public UInt32 SheetSize;
+			public UInt16 NrSheets;
+			public Textures.ImageFormat SheetFormat;
+			public UInt16 SheetRow;
+			public UInt16 SheetLine;
+			public UInt16 SheetWidth;
+			public UInt16 SheetHeight;
+			public UInt32 SheetDataOffset;
+
+			public Byte[][] Sheets;
+
+			public Bitmap GetSheet(int Index)
+			{
+				if (Index >= NrSheets) return null;
+				return Textures.ToBitmap(Sheets[Index], SheetWidth, SheetHeight, SheetFormat);
+			}
+		}
+
+		public CWDH[] CharWidths;
+		public class CWDH
+		{
+			public CWDH(EndianBinaryReader er)
+			{
+				Signature = er.ReadString(Encoding.ASCII, 4);
+				if (Signature != "CWDH") throw new SignatureNotCorrectException(Signature, "CWDH", er.BaseStream.Position - 4);
+				SectionSize = er.ReadUInt32();
+				StartIndex = er.ReadUInt16();
+				EndIndex = er.ReadUInt16();
+				NextCWDHOffset = er.ReadUInt32();
+
+				CharWidths = new CharWidthInfo[EndIndex - StartIndex];
+				for (int i = 0; i < EndIndex - StartIndex; i++) CharWidths[i] = new CharWidthInfo(er);
+			}
+			public String Signature;
+			public UInt32 SectionSize;
+			public UInt16 StartIndex;
+			public UInt16 EndIndex;
+			public UInt32 NextCWDHOffset;
+
+			public CharWidthInfo[] CharWidths;
+		}
+
+		public CMAP[] CharMaps;
+		public class CMAP
+		{
+			public CMAP(EndianBinaryReader er)
+			{
+				Signature = er.ReadString(Encoding.ASCII, 4);
+				if (Signature != "CMAP") throw new SignatureNotCorrectException(Signature, "CMAP", er.BaseStream.Position - 4);
+				SectionSize = er.ReadUInt32();
+				CodeBegin = er.ReadUInt16();
+				CodeEnd = er.ReadUInt16();
+				MappingMethod = er.ReadUInt16();
+				Reserved = er.ReadUInt16();
+				NextCMAPOffset = er.ReadUInt32();
+			}
+			public String Signature;
+			public UInt32 SectionSize;
+			public UInt16 CodeBegin;
+			public UInt16 CodeEnd;
+			public UInt16 MappingMethod;
+			public UInt16 Reserved;
+			public UInt32 NextCMAPOffset;
+		}
+
+		public class CharWidthInfo
+		{
+			public CharWidthInfo(EndianBinaryReader er)
 			{
 				Left = er.ReadByte();
 				GlyphWidth = er.ReadByte();
@@ -101,17 +223,6 @@ namespace _3DS.NintendoWare.FONT
 			public Byte GlyphWidth;
 			public Byte CharWidth;
 		}
-
-		/*public TGLP TextureGlyph;
-		public class TGLP
-		{
-			public TGLP(EndianBinaryReader er)
-			{
-				Signature = er.ReadString(Encoding.ASCII, 4);
-				if (Signature != "TGLP") throw new SignatureNotCorrectException(Signature, "TGLP", er.BaseStream.Position - 4);
-			}
-			public String Signature;
-		}*/
 
 		public class CFNTIdentifier : FileFormatIdentifier
 		{
