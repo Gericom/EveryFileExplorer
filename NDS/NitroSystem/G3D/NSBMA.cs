b7123b4a6c667272a927470de767a41c7a8b7eb9
lwyx2016 <710010028@qq.com>
2017-03-28 08:34:45 -0700

    Realization Some Animations Identification and grouping

---
+++ NDS/NitroSystem/G3D/NSBMA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibEveryFileExplorer.Files;
+using System.Drawing;
+
+namespace NDS.NitroSystem.G3D
+{
+	public class NSBMA : FileFormat<NSBMA.NSBMAIdentifier>
+	{
+		public class NSBMAIdentifier : FileFormatIdentifier
+		{
+			public override string GetCategory()
+			{
+				return Category_Animations;
+			}
+
+			public override string GetFileDescription()
+			{
+				return "Nitro System Binary Material Animation (NSBMA)";
+			}
+
+			public override string GetFileFilter()
+			{
+				return "Nitro System Binary Material Animation (*.nsbma)|*.nsbma";
+			}
+
+			public override Bitmap GetIcon()
+			{
+				return Resource.traffic_light_anim;
+			}
+
+			public override FormatMatch IsFormat(EFEFile File)
+			{
+				if (File.Data.Length > 4 && File.Data[0] == 'B' && File.Data[1] == 'M' && File.Data[2] == 'A' && File.Data[3] == '0') return FormatMatch.Content;
+				return FormatMatch.No;
+			}
+		}
+	}
+}
---
+++ NDS/NitroSystem/G3D/NSBTP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibEveryFileExplorer.Files;
+using System.Drawing;
+
+namespace NDS.NitroSystem.G3D
+{
+	public class NSBTP: FileFormat<NSBTP.NSBTPIdentifier>
+	{
+		public class NSBTPIdentifier : FileFormatIdentifier
+		{
+			public override string GetCategory()
+			{
+                return Category_Animations;
+			}
+
+			public override string GetFileDescription()
+			{
+				return "Nitro System Binary Texture Particles (NSBTP)";
+			}
+
+			public override string GetFileFilter()
+			{
+				return "Nitro System Binary Texture Particles (*.nsbtp)|*.nsbtp";
+			}
+
+			public override Bitmap GetIcon()
+			{
+				return Resource.stack;
+			}
+
+			public override FormatMatch IsFormat(EFEFile File)
+			{
+				if (File.Data.Length > 4 && File.Data[0] == 'B' && File.Data[1] == 'T' && File.Data[2] == 'P' && File.Data[3] == '0') return FormatMatch.Content;
+				return FormatMatch.No;
+			}
+		}
+	}
+}
