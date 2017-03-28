c4b98049981518dc2eb81d054ebea3da8c42211a
lwyx2016 <710010028@qq.com>
2017-03-28 08:14:38 -0700

    Add BFFNT Identification and grouping

---
+++ WiiU/NintendoWare/FONT/FFNT.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibEveryFileExplorer.Files;
+using System.Drawing;
+
+namespace WiiU.NintendoWare.LYT2
+{
+	public class FFNT : FileFormat<FFNT.FFNTIdentifier>
+	{
+		public class FFNTIdentifier : FileFormatIdentifier
+		{
+			public override string GetCategory()
+			{
+                return Category_Fonts;
+            }
+
+			public override string GetFileDescription()
+			{
+				return "Cafe Font (FFNT)";
+			}
+
+			public override string GetFileFilter()
+			{
+				return "Cafe Font (*.bffnt)|*.bffnt";
+			}
+
+			public override Bitmap GetIcon()
+			{
+                return null;
+            }
+
+			public override FormatMatch IsFormat(EFEFile File)
+			{
+				if (File.Data.Length > 4 && File.Data[0] == 'F' && File.Data[1] == 'F' && File.Data[2] == 'N' && File.Data[3] == 'T') return FormatMatch.Content;
+				return FormatMatch.No;
+			}
+
+		}
+	}
+}
