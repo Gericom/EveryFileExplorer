110502ad402b805b1cbc6c108d53155e31d28a78
lwyx2016 <710010028@qq.com>
2017-03-28 08:27:56 -0700

    Add NDS Font NFTR Identification and grouping

---
+++ NDS/NitroSystem/FONT/NFTR.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibEveryFileExplorer.Files;
+using System.Drawing;
+
+namespace NDS
+{
+	public class NFTR:FileFormat<NFTR.NFTRIdentifier>
+	{
+		public class NFTRIdentifier : FileFormatIdentifier
+		{
+			public override string GetCategory()
+			{
+				return Category_Fonts;
+			}
+
+			public override string GetFileDescription()
+			{
+				return "Nitro Fonts Resources (NFTR)";
+			}
+
+			public override string GetFileFilter()
+			{
+				return "Nitro Fonts Resources (*.nftr)|*.nftr";
+			}
+
+			public override Bitmap GetIcon()
+			{
+                return null;
+            }
+
+			public override FormatMatch IsFormat(EFEFile File)
+			{
+				if (File.Data.Length > 4 && File.Data[0] == 'R' && File.Data[1] == 'T' && File.Data[2] == 'F' && File.Data[3] == 'N') return FormatMatch.Content;
+				return FormatMatch.No;
+			}
+		}
+	}
+}
