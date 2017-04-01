05b76a6b1ccc17de8ec7d85520202dfcd0fefad7
lwyx2016 <710010028@qq.com>
2017-04-01 10:32:40 -0700

    Start Working CTR Texture Package Format

---
+++ 3DS/CTPK.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibEveryFileExplorer.Files;
+using System.Drawing;
+
+namespace _3DS
+{
+    public class CTPK:FileFormat<CTPK.CTPKIdentifier>
+    {
+        public class CTPKIdentifier : FileFormatIdentifier
+        {
+            public override string GetCategory()
+            {
+                return Category_Archives;
+
+            }
+
+            public override string GetFileDescription()
+            {
+                return "CTR Texture Package (CTPK)";
+            }
+
+            public override string GetFileFilter()
+            {
+                return "CTR Texture Package (*.ctpk)|*.ctpk";
+            }
+
+            public override Bitmap GetIcon()
+            {
+                return null;
+            }
+
+            public override FormatMatch IsFormat(EFEFile File)
+            {
+                if (File.Data.Length > 4 && File.Data[0] == 'C' && File.Data[1] == 'T' && File.Data[2] == 'P' && File.Data[3] == 'K') return FormatMatch.Content;
+                return FormatMatch.No;
+            }
+
+        }
+    }
+}
