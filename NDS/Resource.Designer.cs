706e928c3daa9231d70db1f51bf46021460fd78b
lwyx2016 <710010028@qq.com>
2017-04-03 23:49:21 -0700

    Update Resources File resx

--- NDS/Resource.Designer.cs
+++ NDS/Resource.Designer.cs
@@ -279,5 +279,15 @@ namespace NDS {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap water {
+            get {
+                object obj = ResourceManager.GetObject("water", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
