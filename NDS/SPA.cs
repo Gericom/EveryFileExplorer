d784e08d3ab3d7772b0aeb5d85124247b131c823
lwyx2016 <710010028@qq.com>
2017-04-04 15:22:50 +0800

    Add NDS Particles SPA Identification and grouping

---
+++ NDS/SPA.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibEveryFileExplorer.Files;
+using System.Drawing;
+using LibEveryFileExplorer.IO;
+
+namespace NDS
+{
+    public class SPA:FileFormat<SPA.SPAIdentifier>
+    {
+        public class SPAIdentifier : FileFormatIdentifier
+        {
+            public override string GetCategory()
+            {
+                return Category_Particles;
+            }
+
+            public override string GetFileDescription()
+            {
+                return "Nitro System Particles Archive (SPA)";
+            }
+
+            public override string GetFileFilter()
+            {
+                return "Nitro System Particles Archive (*.spa)|*.spa";
+            }
+
+            public override Bitmap GetIcon()
+            {
+                return Resource.water;
+            }
+
+            public override FormatMatch IsFormat(EFEFile File)
+            {
+                if (File.Name.ToLower().EndsWith(".spa")) return FormatMatch.Extension;
+                return FormatMatch.No;
+            }
+
+        }
+    }
+}
