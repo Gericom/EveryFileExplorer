e2032d7deb56091035b268e7ed306a169a0f955a
lwyx2016 <710010028@qq.com>
2017-06-04 15:32:00 +0800

    Add part CTPK The Code（There's another part I haven't submitted yet）

--- 3DS/CTPK.cs
+++ 3DS/CTPK.cs
@@ -5,14 +5,53 @@ using System.Text;
 using LibEveryFileExplorer.Files;
 using System.Drawing;
 using System.Windows.Forms;
+using LibEveryFileExplorer.IO;
+using System.IO;
 
 namespace _3DS
 {
-    public class CTPK:FileFormat<CTPK.CTPKIdentifier>, IViewable
+    public class CTPK : FileFormat<CTPK.CTPKIdentifier>, IViewable
     {
+        public CTPK(byte[] Data)
+        {
+            EndianBinaryReader er = new EndianBinaryReader(new MemoryStream(Data), Endianness.LittleEndian);
+            try
+            {
+                Header = new CTPKHeader(er);
+            }
+            finally
+            {
+                er.Close();
+            }
+        }
         public Form GetDialog()
         {
             return new Form();
+        }
+
+        public CTPKHeader Header;
+        public class CTPKHeader
+        {
+            public CTPKHeader(EndianBinaryReader er)
+            {
+                Signature = er.ReadString(Encoding.ASCII, 4);
+                if (Signature != "CTPK") throw new SignatureNotCorrectException(Signature, "CTPK", er.BaseStream.Position - 4);
+                Endianness = er.ReadUInt16();
+                HeaderSize = er.ReadUInt16();
+                Version = er.ReadUInt32();
+                FileSize = er.ReadUInt32();
+                FileTableOffset = er.ReadUInt32();
+                FileTableLength = er.ReadUInt32();
+                FileDataOffset = er.ReadUInt32();
+            }
+            public String Signature;
+            public UInt16 Endianness;
+            public UInt16 HeaderSize;
+            public UInt32 Version;
+            public UInt32 FileSize;
+            public UInt32 FileTableOffset;
+            public UInt32 FileTableLength;
+            public UInt32 FileDataOffset;
         }
 
         public class CTPKIdentifier : FileFormatIdentifier
