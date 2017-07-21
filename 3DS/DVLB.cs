cdc5ee10604349976d0d42a85c2c70af3462bb37
lwyx2016 <710010028@qq.com>
2017-07-22 02:26:39 +0800

    Make some changes

--- 3DS/DVLB.cs
+++ 3DS/DVLB.cs
@@ -5,6 +5,8 @@ using System.Text;
 using LibEveryFileExplorer.Files;
 using System.Drawing;
 using System.Windows.Forms;
+using LibEveryFileExplorer.IO;
+using System.IO;
 
 namespace _3DS
 {
@@ -13,6 +15,44 @@ namespace _3DS
         public Form GetDialog()
         {
             return new Form();
+        }
+
+        public DVLB(byte[] Data)
+        {
+            EndianBinaryReader er = new EndianBinaryReader(new MemoryStream(Data), Endianness.LittleEndian);
+            try
+            {
+                Header = new DVLBHeader(er);
+            }
+            finally
+            {
+                er.Close();
+            }
+        }
+
+        public DVLBHeader Header;
+        public class DVLBHeader
+        {
+            public DVLBHeader(EndianBinaryReader er)
+            {
+                Signature = er.ReadString(Encoding.ASCII, 4);
+                if (Signature != "DVLB") throw new SignatureNotCorrectException(Signature, "DVLB", er.BaseStream.Position - 4);
+                Endianness = er.ReadUInt16();
+                HeaderSize = er.ReadUInt16();
+                Version = er.ReadUInt32();
+                FileSize = er.ReadUInt32();
+                FileTableOffset = er.ReadUInt32();
+                FileTableLength = er.ReadUInt32();
+                FileDataOffset = er.ReadUInt32();
+            }
+            public String Signature;
+            public UInt16 Endianness;
+            public UInt16 HeaderSize;
+            public UInt32 Version;
+            public UInt32 FileSize;
+            public UInt32 FileTableOffset;
+            public UInt32 FileTableLength;
+            public UInt32 FileDataOffset;
         }
 
         public class DVLBIdentifier : FileFormatIdentifier
