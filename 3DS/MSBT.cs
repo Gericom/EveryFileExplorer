83f7a7d6f1db2fc4c0b69ca7fef776886143f11b
lwyx2016 <710010028@qq.com>
2017-07-22 02:22:28 +0800

    Make some changes

--- 3DS/MSBT.cs
+++ 3DS/MSBT.cs
@@ -4,13 +4,58 @@ using System.Linq;
 using System.Text;
 using LibEveryFileExplorer.Files;
 using System.Drawing;
+using System.Windows.Forms;
+using LibEveryFileExplorer.IO;
+using System.IO;
 
-/*namespace _3DS
+namespace _3DS
 {
-	public class MSBT:FileFormat<MSBT.MSBTIdentifier>
-	{
-		public class MSBTIdentifier:FileFormatIdentifier
-		{
+	public class MSBT:FileFormat<MSBT.MSBTIdentifier>, IViewable
+    {
+            public Form GetDialog()
+            {
+                return new Form();
+            }
+        public MSBT(byte[] Data)
+        {
+            EndianBinaryReader er = new EndianBinaryReader(new MemoryStream(Data), Endianness.LittleEndian);
+            try
+            {
+                Header = new MSBTHeader(er);
+            }
+            finally
+            {
+                er.Close();
+            }
+        }
+
+        public MSBTHeader Header;
+            public class MSBTHeader
+            {
+                public MSBTHeader(EndianBinaryReader er)
+                {
+                    Signature = er.ReadString(Encoding.ASCII, 8);
+                    if (Signature != "MsgStdBn") throw new SignatureNotCorrectException(Signature, "MsgStdBn", er.BaseStream.Position - 8);
+                    Endianness = er.ReadUInt16();
+                    HeaderSize = er.ReadUInt16();
+                    Version = er.ReadUInt32();
+                    FileSize = er.ReadUInt32();
+                    FileTableOffset = er.ReadUInt32();
+                    FileTableLength = er.ReadUInt32();
+                    FileDataOffset = er.ReadUInt32();
+                }
+                public String Signature;
+                public UInt16 Endianness;
+                public UInt16 HeaderSize;
+                public UInt32 Version;
+                public UInt32 FileSize;
+                public UInt32 FileTableOffset;
+                public UInt32 FileTableLength;
+                public UInt32 FileDataOffset;
+            }
+
+        public class MSBTIdentifier : FileFormatIdentifier
+        {
             public override string GetCategory()
             {
                 return "MSBT Text";
@@ -33,10 +78,10 @@ using System.Drawing;
 
             public override FormatMatch IsFormat(EFEFile File)
 			{
-                if (File.Name.ToLower().EndsWith(".msbt")) return FormatMatch.Extension;
+                if (File.Data.Length > 10 && File.Data[0] == 'M' && File.Data[1] == 's' && File.Data[2] == 'g' && File.Data[3] == 'S' && File.Data[4] == 't' && File.Data[5] == 'd' && File.Data[6] == 'B' && File.Data[7] == 'n') return FormatMatch.Content;
                 return FormatMatch.No;
             }
 
         }
     }
-}*/
+}
